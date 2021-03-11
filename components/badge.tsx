1cadbf6997c47cd3c3ce3dd2435790e12629a970
Alexander Eckert <eckertalex@protonmail.com>
2021-03-11 11:07:57 +0100

    feat: remove useless import

--- components/badge.tsx
+++ components/badge.tsx
@@ -1,6 +1,3 @@
-import React from 'react'
-/* -------------------------------------------------------------------------- */
-
 export function Badge(props: React.PropsWithChildren<unknown>) {
   const {children} = props
 
