9dd51fcd8d808eafe9df69924842539c42ae4474
Alexander Eckert <eckertalex@protonmail.com>
2021-03-14 16:26:35 +0100

    feat: document my keyboard and setup

---
+++ pages/keyboard.jsx
@@ -0,0 +1,37 @@
+import {getFileBySlug} from '@/lib/mdx'
+import siteMetadata from '@/data/siteMetadata'
+import hydrate from 'next-mdx-remote/hydrate'
+import {MDXComponents} from '@/components/mdx-components'
+import {PageSeo} from '@/components/seo'
+import {PageTitle} from '@/components/page-title'
+
+export default function Keyboard({keyboard}) {
+  const {mdxSource, frontMatter} = keyboard,
+    content = hydrate(mdxSource, {
+      components: MDXComponents,
+    })
+
+  return (
+    <>
+      <PageSeo
+        description={`${frontMatter.title} - ${siteMetadata.author}`}
+        title={`${frontMatter.title} - ${siteMetadata.author}`}
+        url={`${siteMetadata.siteUrl}/keyboard`}
+      />
+      <div className="divide-y">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <PageTitle>{frontMatter.title}</PageTitle>
+        </div>
+        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+          <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">{content}</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const keyboard = await getFileBySlug('keyboard')
+
+  return {props: {keyboard}}
+}
