2282e0dedbbf38619c97e2f9e83e576f15c96515
Alexander Eckert <eckertalex@protonmail.com>
2021-08-18 21:04:48 +0200

    feat: add search to readings

--- src/pages/reading.tsx
+++ src/pages/reading.tsx
@@ -1,20 +1,47 @@
-import {VStack} from '@chakra-ui/react'
+import React from 'react'
+import {Input, InputGroup, InputRightElement, VStack} from '@chakra-ui/react'
+import {Search as SearchIcon} from 'lucide-react'
 import metadata from 'metadata'
 import {PageSeo} from 'features/seo/seo'
 import {PageTitle} from 'layout/page-title'
 import {ReadingList} from 'features/reading-list/reading-list'
 import {fetchReadings, Reading} from 'lib/gitrows'
 
-export default function ReadingPage({readings}: {readings: Record<string, Reading[]>}) {
+function match(searchValue: string, reading: Reading) {
   return (
-    <VStack alignItems="start" spacing={8}>
+    (reading.description ?? '').toLowerCase().includes(searchValue.toLowerCase()) ||
+    (reading.title ?? '').toLowerCase().includes(searchValue.toLowerCase()) ||
+    (reading.author ?? '').toLowerCase().includes(searchValue.toLowerCase())
+  )
+}
+
+function filterReadings(searchValue: string, allReadings: Record<string, Reading[]>): Record<string, Reading[]> {
+  return Object.fromEntries(
+    Object.entries(allReadings)
+      .map(([day, readings]) => [day, readings.filter((reading) => match(searchValue, reading))])
+      .filter(([, readings]) => readings.length > 0)
+  )
+}
+
+export default function ReadingPage({readings}: {readings: Record<string, Reading[]>}) {
+  const [searchValue, setSearchValue] = React.useState('')
+  const filteredReadings = filterReadings(searchValue, readings)
+
+  return (
+    <VStack alignItems="start" marginBottom={8}>
       <PageSeo
         description="All the blog posts I have read since June 2020."
         title={`Reading - ${metadata.author}`}
         url={`${metadata.siteUrl}/reading`}
       />
       <PageTitle as="h1">Reading</PageTitle>
-      <ReadingList readings={readings} />
+      <InputGroup maxWidth="lg">
+        <Input placeholder="Search posts" onChange={(e) => setSearchValue(e.target.value)} value={searchValue} />
+        <InputRightElement>
+          <SearchIcon />
+        </InputRightElement>
+      </InputGroup>
+      <ReadingList readings={filteredReadings} />
     </VStack>
   )
 }
