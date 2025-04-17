3a5995d820f78ba4d459500f20c31ea26dfdc413
Alexander Eckert <eckertalex@proton.me>
2025-04-18 00:51:22 +0200

    add seo

--- src/content/siteConfig.ts
+++ src/content/siteConfig.ts
@@ -3,10 +3,13 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   site: "https://eckertalex.dev",
   name: "eckertalex.dev",
+  author: "Alexander Eckert",
   slogan: "A developer who cares about the user.",
   description: `Hey, I'm Alexander. I am a software developer, father of 2, and husband to a wonderful wife. A developer who cares about the user.`,
   copyright: "All rights reserved © Alexander Eckert 2025",
-  ogImage: "http://eckertalex.dev/og.png",
+  ogImage: "/og.png",
+  ogImageAlt:
+    "Profile card for Alexander Eckert with the tagline 'A developer who cares about the user' and a pink hexagonal logo on a dark background.",
   links: {
     github: "https://github.com/eckertalex",
     linkedin: "https://linkedin.com/in/eckertalex",
